2643ad2a2401bb0cb972850ee178e3b6d1b3e5f3
mcspringroll <mcspringroll@gmail.com>
2017-01-05 19:40:59 -0600

    Built, removed some unused imports.

--- Dungeon/Generation/GenerationCommand.cs
+++ Dungeon/Generation/GenerationCommand.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using DungeonAPI.Definitions;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DungeonAPI.Generation
 {
