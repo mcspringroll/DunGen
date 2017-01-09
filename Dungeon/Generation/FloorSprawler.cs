1493f05ff068885bb81e7c377e8b65c3b1ad084d
chchel <charlienorton496@gmail.com>
2017-01-08 17:39:05 -0800

    New shuffle method and helper methods (commented), wrote constructor for floor sprawler node that takes a room as a param, used this in addRoom, changed EndNode == null to DoesNotHaveNextRoom in addRoom, did the line ending thing which seems pointless but w/e

--- Dungeon/Generation/FloorSprawler.cs
+++ Dungeon/Generation/FloorSprawler.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using DungeonAPI.Definitions;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DungeonAPI.Generation
 {
@@ -79,7 +75,7 @@ namespace DungeonAPI.Generation
         {
             get
             {
-                return EndNode == null;
+                return !HasNextRoom;
             }
         }
 
@@ -142,15 +138,13 @@ namespace DungeonAPI.Generation
         }
 
         /// <summary>
-        /// Addes a new room to the 
+        /// Adds a new room
         /// </summary>
         /// <param name="toAdd"></param>
         public void addRoom(Room toAdd)
         {
-            Node temp = new Node();
-            temp.Data = toAdd;
-            NumberOfRooms++;
-            if (EndNode == null)
+            Node temp = new Node(toAdd);
+            if (DoesNotHaveNextRoom)
             {
                 FrontNode = EndNode = temp;
             }
@@ -160,6 +154,7 @@ namespace DungeonAPI.Generation
                 temp.Previous = EndNode;
                 EndNode = temp;
             }
+            NumberOfRooms++;
         }
 
         /// <summary>
@@ -205,6 +200,75 @@ namespace DungeonAPI.Generation
         }
 
         /// <summary>
+        /// Puts each room randomly into one of two new sprawlers, then
+        /// takes from a random sprawler with random IsDepthFirst to rebuild 
+        /// original sprawler. Shuffled sprawler stored in this object.
+        /// Should be order n on the number of rooms, no idea how this
+        /// will perform in terms of shuffling
+        /// </summary>
+        public void shuffleRooms2()
+        {
+            //The new sprawlers to be used
+            FloorSprawler newSprawler1 = new FloorSprawler();
+            FloorSprawler newSprawler2 = new FloorSprawler();
+            //Stores which sprawler is currently in use
+            FloorSprawler temp;
+            //Saves number of rooms b/c remove method is used
+            int originalNumberOfRooms = this.NumberOfRooms;
+            //Stores which sprawler to use
+            bool sprawlerChoice;
+            //Stores whether sprawler in use should be depth first
+            bool sprawlerDepthFirst;
+            //Random number generator, used throughout method
+            Random r = new Random();
+
+            //Executes until this has been emptied
+            while (this.NumberOfRooms > 0)
+            {
+                //Picks one sprawler
+                sprawlerChoice = randBool(r);
+                temp = pickSprawler(sprawlerChoice, newSprawler1, newSprawler2);
+                //Takes a room from this and puts it in that sprawler
+                temp.addRoom(this.removeNextRoom());
+            }
+            //Executes until this is refilled
+            while(this.NumberOfRooms < originalNumberOfRooms)
+            {
+                //Picks a random sprawler, sets random IsDepthFirst
+                sprawlerChoice = randBool(r);
+                sprawlerDepthFirst = randBool(r);
+                temp = pickSprawler(sprawlerChoice, newSprawler1, newSprawler2);
+                temp.IsDepthFirst = sprawlerDepthFirst;
+                //Takes a room from that sprawler and puts it in this
+                this.addRoom(temp.removeNextRoom());
+            }
+        }
+
+        /// <summary>
+        /// Helper method to pick one sprawler based on a boolean.
+        /// </summary>
+        /// <param name="choice"> the boolean </param>
+        /// <param name="s1"> the first sprawler </param>
+        /// <param name="s2"> the second sprawler </param>
+        /// <returns> the sprawler that was picked </returns>
+        private FloorSprawler pickSprawler(bool choice, FloorSprawler s1, FloorSprawler s2)
+        {
+            if (choice)
+                return s1;
+            return s2;
+        }
+
+        /// <summary>
+        /// Helper method to get a random boolean (slightly skewed)
+        /// </summary>
+        /// <param name="r"> the Random object to use </param>
+        /// <returns> a random boolean </returns>
+        private bool randBool(Random r)
+        {
+            return (r.NextDouble() < .5);
+        }
+
+        /// <summary>
         /// Gets a random byte.
         /// </summary>
         /// <returns></returns>
@@ -232,6 +296,14 @@ namespace DungeonAPI.Generation
         /// </summary>
         private class Node
         {
+            /// <summary>
+            /// Constructs a node with the given room data.
+            /// </summary>
+            public Node(Room data)
+            {
+                Data = data;
+            }
+
             /// <summary>
             /// The node "in front of" this node.
             /// </summary>
